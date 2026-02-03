8a350bbd78edd680ec8e8e66cadc9b2640f3ae68
solixx144 <mmuhammettalha27@gmail.com>
2026-02-03 20:01:36 +0300

    Change popup display style to flex

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const points = document.querySelectorAll(".point");
 const popup = document.getElementById("popup");
 const popupTitle = document.getElementById("popupTitle");
+const closeBtn = document.getElementById("closePopup");
 
 points.forEach(point => {
   point.addEventListener("click", () => {
     popupTitle.textContent = point.dataset.name;
-    popup.style.display = "block";
+    popup.style.display = "flex";
   });
 });
 
-function closePopup() {
+closeBtn.addEventListener("click", () => {
   popup.style.display = "none";
-}
+});
